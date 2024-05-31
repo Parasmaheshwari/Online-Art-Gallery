c20698faffe5adce57de2c696c059e5ede14210d
Parasmaheshwari <124872333+Parasmaheshwari@users.noreply.github.com>
2024-05-31 23:26:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal(imgUrl) {
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("modalImg").src = imgUrl;
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
